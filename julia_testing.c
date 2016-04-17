f9e87139dad8b02127b9e2de363a410919b7b026
Scott King <kingsc850@gmail.com>
2016-04-17 11:34:03 -0400

    some minor refactoring, global colour array working - need to write a script for the entire process

--- julia_testing.c
+++ julia_testing.c
@@ -2,16 +2,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
-struct Colour {
+void display(void);
+void initialize(void);
+
+// Struct for colour of every pixel
+typedef struct {
   int r;
   int g;
   int b;
-};
+} Colour;
+Colour *colours = NULL;     // Globally initialize colour array
 
-struct Colour colours[49];
+int main(int argc, char ** argv) {
+  colours = malloc((atoi(argv[1]) - 1) * sizeof(*colours));      // Allocate array of random colours
 
-void myinit(void) {
+  srand((unsigned) time(NULL));
+  for (int i = 0; i < atoi(argv[1]); i++) {
+    colours[i].r = rand() % 255;
+    colours[i].g = rand() % 255;
+    colours[i].b = rand() % 255;
+  }
+
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(1000, 1000);
+  glutInitWindowPosition(0, 0);
+  glutCreateWindow("High Definition Julia Set");
+  glutDisplayFunc(display);
+  initialize();
+  glutMainLoop();
+
+  free(colours);
+  return 0;
+}
+
+// void defineColourArray(Colour **array, int mag) {
+//   *array = malloc((mag - 1) * sizeof(*array));
+// }
+
+// Sets up some OpenGL nonsense
+void initialize(void) {
   glClearColor(1.0, 1.0, 1.0, 1.0);
   glColor3f(1.0, 0.0, 0.0);
   glMatrixMode(GL_PROJECTION);
@@ -20,47 +52,34 @@ void myinit(void) {
   glMatrixMode(GL_MODELVIEW);
 }
 
+// This fn is called by glutDisplayFunc and is what's used to send output to OpenGL shizan
 void display(void) {
   typedef GLfloat point2[2];
   int j = 0;
+  Colour current;
   point2 p = {0.0, 0.0};
 
   char *piece; char line[20]; char *array[3];
   FILE *file = fopen("nope.txt", "r");
 
+  // Loops for the length of the file, plotting the points with the respective colours
   while (fgets(line, sizeof(line), file)) {
     piece = strtok(line, " ");
     while (piece != NULL) {
       array[j++] = piece;
       piece = strtok(NULL, " ");
     }
-
     j = 0;
 
     p[0] = atof(array[1]);
     p[1] = atof(array[0]);
+    current = colours[atoi(array[2])];
+
 
     glBegin(GL_POINTS);
-    glColor3ub(colours[atoi(array[2])].r, colours[atoi(array[2])].g, colours[atoi(array[2])].b);
+    glColor3ub(current.r, current.g, current.b);
     glVertex2fv(p);
     glEnd();
   }
   glFlush();
-}
-
-void main(int argc, char ** argv) {
-  for (int i = 0; i < atoi(argv[1]); i++) {
-    colours[i].r = rand() % 255;
-    colours[i].g = rand() % 255;
-    colours[i].b = rand() % 255;
-  }
-
-  glutInit(&argc, argv);
-  glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize (1000, 1000);
-  glutInitWindowPosition (0, 0);
-  glutCreateWindow ("Stuff by scott");
-  glutDisplayFunc (display);
-  myinit();
-  glutMainLoop();
 }
